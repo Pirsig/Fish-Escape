0e9ea6467a649ed6bf98d885d96afe1c04a24562
Pirsig <assortedchaos@gmail.com>
2020-02-19 20:27:25 -0700

    created LoadSliderFromPlayerPrefs which loads a float value from the given key and assigns it to the slider's value when it's enabled.

---
+++ Assets/Scripts/UI/LoadSliderFromPlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSliderFromPlayerPrefs : MonoBehaviour
+{
+    public string keyToLoad; 
+
+    private void OnEnable()
+    {
+        Slider slider = GetComponentInChildren<Slider>();
+        slider.value = PlayerPrefs.GetFloat(keyToLoad);
+    }
+}
