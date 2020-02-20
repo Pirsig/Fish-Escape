571d27aa12ab8dc885860c10dcaccffc26ecdc2e
Pirsig <assortedchaos@gmail.com>
2020-02-19 21:07:24 -0700

    created script for loading scene via UI button and taking in the scene string name

---
+++ Assets/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
