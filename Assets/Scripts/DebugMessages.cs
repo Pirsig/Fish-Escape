faa646098694779aa1a8982df7b682cffd451d45
Pirsig <assortedchaos@gmail.com>
2020-02-19 22:40:57 -0700

    -added new DebugMessages class to hold some common console messages being used -three debug messages related to events, ClassInObjectSubscribed, ClassInObjectUnsubscribed, EventFired

---
+++ Assets/Scripts/DebugMessages.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugMessages
+{
+    public static void ClassInObjectSubscribed(Component caller, string eventName)
+    {
+        Debug.Log(caller.GetType().Name + " in " + caller.gameObject.name + " is subscribed to " + eventName + '.');
+    }
+
+    public static void ClassInObjectUnsubscribed(Component caller, string eventName)
+    {
+        Debug.Log(caller.GetType().Name + " in " + caller.gameObject.name + " is unsubscribed to " + eventName + '.');
+    }
+
+    public static void EventFired(Component caller, string eventName)
+    {
+        Debug.Log(eventName + " in " + caller.gameObject.name + " has fired.");
+    }
+}
