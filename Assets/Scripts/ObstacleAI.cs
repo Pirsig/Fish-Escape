c2ca046b54a99b5d0c6de8ad42875523c2696390
Pirsig <assortedchaos@gmail.com>
2020-02-19 21:59:23 -0700

    broke out obstacle respawning into separate methods

--- Assets/Scripts/ObstacleAI.cs
+++ Assets/Scripts/ObstacleAI.cs
@@ -13,20 +13,39 @@ public class ObstacleAI : MonoBehaviour
     [SerializeField]
     private float randomOffset = 2f;  //used as a range of negative to positive to offset the heighth of the obstacle
 
+    private bool playerDead;
+
     void Update()
     {
-        transform.position += Time.deltaTime * speed * Vector3.left;
+        if (!playerDead)
+        {
+            transform.position += Time.deltaTime * speed * Vector3.left;
+        }
+        
+
         if (transform.position.x <= despawnPosition)
         {
             if (randomOffset == 0)
             {
-                transform.position = new Vector3(respawnPosition, transform.position.y, transform.position.z);
+                RespawnObstacle();
             }
             else
             {
-                float randomHeight = UnityEngine.Random.Range(-randomOffset, randomOffset);
-                transform.position = new Vector3(respawnPosition, randomHeight, transform.position.z);
+                RespawnObstacle(randomOffset);
             }
         }
+    }
+
+    //moves the obstacle back to it's starting position along the x axis
+    private void RespawnObstacle()
+    {
+        transform.position = new Vector3(respawnPosition, transform.position.y, transform.position.z);
+    }
+
+    //moves the obstacle back to it's starting position along the x axis with a new random heighth in the range given
+    private void RespawnObstacle(float respawnRandomOffset)
+    {
+        float randomHeight = Random.Range(-respawnRandomOffset, respawnRandomOffset);
+        transform.position = new Vector3(respawnPosition, randomHeight, transform.position.z);
     }
 }
