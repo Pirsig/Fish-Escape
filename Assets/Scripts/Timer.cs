abd69e7421db95e50061d964d84f19c9cf969fbf
Pirsig <assortedchaos@gmail.com>
2020-02-22 23:40:38 -0700

    -added simple Timer class -stores current and end times, and whether a timer has completed or not -must be updated manually either in update with Time.DeltaTime or a custom elapsed time -can simply be reset or reset with a new maxTime

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer //: MonoBehaviour
+{
+    private float currentTime;
+    private float maxTime;
+    private bool timerCompleted;
+
+    public float CurrentTime { get => currentTime; }
+    public float MaxTime { get => maxTime; }
+    public bool TimerCompleted { get => timerCompleted;  }
+
+    public Timer(float endTime)
+    {
+        currentTime = 0;
+        maxTime = endTime;
+        timerCompleted = false;
+    }
+
+    //Updates the timer and checks if it is completed.
+    //Use Time.DeltaTime for elapsedTime to use UpdateTimer in frame update.
+    //Use custom values when not keeping track of the timer in Update()
+    public void UpdateTimer(float elapsedTime)
+    {
+        currentTime += elapsedTime;
+        if(currentTime >= maxTime)
+        {
+            timerCompleted = true;
+        }
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = 0;
+        timerCompleted = false;
+    }
+
+    public void ResetTimer(float newEndTime)
+    {
+        currentTime = 0;
+        timerCompleted = false;
+        maxTime = newEndTime;
+    }
+}
