2b6300a74c47df1bf3f5e035d66aee49522e4264
Pirsig <assortedchaos@gmail.com>
2020-02-23 16:22:13 -0700

    added StringVariable and StringReference to BaseVariables

---
+++ Assets/Scripts/_BaseVariables/StringReference.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BaseVariables
+{
+    [Serializable]
+    public class StringReference
+    {
+        public bool isLocal = true;
+        public bool isStatic = true;
+        public string localValue;
+        public StringVariable Variable;
+
+        public StringReference()
+        { }
+
+        public StringReference(string value)
+        {
+            isLocal = true;
+            isStatic = true;
+            localValue = value;
+        }
+
+        public string Value
+        {
+            get { return isLocal ? localValue : Variable.value; }
+            set
+            {
+                if (!isStatic)
+                {
+                    if (!isLocal)
+                    {
+                        Variable.ChangeText(value);
+                    }
+                    else
+                    {
+                        localValue = value;
+                    }
+                }
+            }
+        }
+
+        public static implicit operator string(StringReference reference)
+        {
+            return reference.Value;
+        }
+
+        public override string ToString()
+        {
+            return isLocal ? localValue.ToString() : Variable.value.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/_BaseVariables/StringVariable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace BaseVariables
+{
+    [CreateAssetMenu(menuName = "BaseVariables/StringVariable")]
+    public class StringVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string developerDescription = "";
+#endif
+        public string value;
+
+        //constructor with given float value for use with conversion
+        public StringVariable(string newValue)
+        {
+            value = newValue;
+        }
+
+        public void ChangeText(string newValue)
+        {
+            value = newValue;
+        }
+
+        public void ChangeText(StringVariable newValue)
+        {
+            value = newValue.value;
+        }
+
+        public void EmptyText()
+        {
+            value = "";
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+
+        public static implicit operator string(StringVariable variable)
+        {
+            return variable.value;
+        }
+    }
+}
