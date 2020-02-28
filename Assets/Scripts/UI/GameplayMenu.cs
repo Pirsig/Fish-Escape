26680cb489b392cea89e86443c0da7d7fd2836f9
Pirsig <assortedchaos@gmail.com>
2020-02-28 03:04:25 -0700

    -changed LoadSceneButton to GameplayMenu to more accurately reflect its role -added protection from a null reference error that can be cause by running things with GameplayMenu attached when there is no BGMController present

---
+++ Assets/Scripts/UI/GameplayMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayMenu : MonoBehaviour
+{
+    private BGMController bgmController;
+    //used for null exception protection
+    private bool hasBGMController;
+
+    private void Awake()
+    {
+        bgmController = GameObject.FindObjectOfType<BGMController>();
+        //bgmController = GameObject.Find("BGMController").GetComponent<BGMController>();
+        if (bgmController != null)
+        {
+            hasBGMController = true;
+        }
+        else
+        {
+            hasBGMController = false;
+            Debug.LogWarning("Gameplay Menu did not find a BGMController");
+        }
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ChangeMusic(int trackNumber)
+    {
+        //Only changes the music if we have a reference to a BGMController so we can't accidentally cause a null reference exception
+        if(hasBGMController)
+            bgmController.ChangeMusicTrack(trackNumber);
+    }
+
+    public void QuitButton()
+    {
+        PlayerPrefs.Save();
+        Application.Quit();
+        Debug.Log("QuitGame button pressed");
+    }
+}
