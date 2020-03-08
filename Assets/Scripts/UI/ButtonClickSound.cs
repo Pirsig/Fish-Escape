3483dcf81a69eccb41a5d114dd9f7fbdf40498f8
Pirsig <assortedchaos@gmail.com>
2020-03-08 12:20:06 -0600

    -balancing and reorganizing

---
+++ Assets/Scripts/UI/ButtonClickSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClickSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip sound;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource audioSource { get { return GetComponent<AudioSource>(); } }
+
+    private void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = sound;
+
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    private void PlaySound()
+    {
+        audioSource.PlayOneShot(sound);
+        Debug.LogWarning("Click Sound");
+    }
+}
