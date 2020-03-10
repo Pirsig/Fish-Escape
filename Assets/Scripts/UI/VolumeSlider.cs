110602b16438b572d9fc29f315c65ba432cb6855
Pirsig <assortedchaos@gmail.com>
2020-03-10 15:45:49 -0600

    -LoadSliderFromPlayerPrefs is now VolumeSlider, better describing it's specific function -everything to make the slider work is now contained in this script instead of having to use inspector events and OptionsMenu

---
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using BaseVariables;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerGroup mixerGroup;
+    private AudioMixer mixer { get => mixerGroup.audioMixer; }
+    [SerializeField]
+    private StringReference keyToLoad;
+
+    private Slider slider;
+
+    private void OnEnable()
+    {
+        slider = GetComponentInChildren<Slider>();
+        slider.onValueChanged.AddListener(OnValueChanged);
+        slider.value = PlayerPrefs.GetFloat(keyToLoad);
+    }
+
+    private void OnDisable()
+    {
+        slider.onValueChanged.RemoveListener(OnValueChanged);
+    }
+
+    private void OnValueChanged(float volume)
+    {
+        mixer.SetFloat(keyToLoad, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(keyToLoad, volume);
+        Debug.Log("volume = " + volume.ToString());
+    }
+}
